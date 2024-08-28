04882128163f5243dd65f61d2fcbf488912d7c32
Regan Tam <regantam92@gmail.com>
2024-08-28 10:57:20 +1000

    Added recommended items and footer

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -11,19 +11,40 @@ import {
   showToast,
 } from "../components/common/toastNotification";
 import BreakCrumbNavigation from "../components/navigation/breadCrumbNavigation";
+import Scroller from "../components/common/scroller/scroller";
+import { selectRandomNumbersArray } from "../utils/calculationUtils";
+import Footer from "../components/navigation/footer";
 
 function Product() {
   const { productId } = useParams<{
     productId: string;
   }>();
   const [product, setProduct] = useState<productType | undefined>(undefined);
-
+  const [recommended, setRecommended] = useState<productType[]>([]);
   useEffect(() => {
     const selectedProduct = products.find(
       (product) => productId === getLastProductParamLink(product.href)
     );
     setProduct(selectedProduct);
+
+    setRecommended(fetchStoreSales(selectedProduct));
   }, [productId]);
+
+  function fetchStoreSales(product: productType) {
+    const categoryItems = products.filter(
+      (item) => item.subCategory === product.subCategory
+    );
+    console.log(categoryItems);
+    const itemIndex = selectRandomNumbersArray(
+      categoryItems.length,
+      categoryItems.length > 8 ? 8 : categoryItems.length
+    );
+    let result = [];
+    itemIndex.forEach((index) => {
+      result = [...result, categoryItems[index]];
+    });
+    return result;
+  }
 
   function handleToastNotification(
     message: string,
@@ -38,35 +59,41 @@ function Product() {
 
   return (
     <>
-      <ToastNotification position="bottom-center" autoClose={3000} />
-      <div className="content">
-        <div className="top-bar">
-          <BreakCrumbNavigation />
+      <div className="product">
+        <ToastNotification position="bottom-center" autoClose={3000} />
+        <div className="content">
+          <div className="top-bar">
+            <BreakCrumbNavigation />
+          </div>
+        </div>
+        <div className="content product">
+          <div className="col-left">
+            <ImageDisplay
+              image={product.image}
+              imageAlt={product.imageAlt}
+              discount={product.discount}
+            />
+          </div>
+          <div className="col-right">
+            <InformationList
+              title={product.title}
+              price={product.price}
+              discount={product.discount}
+              salePrice={product.salePrice}
+              rating={product.rating}
+              description={product.description}
+            />
+            <ProductForm
+              product={product}
+              handleToast={handleToastNotification}
+            />
+          </div>
+        </div>
+        <div className="content">
+          <Scroller items={[...recommended]} title="Recommended" size="large" />
         </div>
       </div>
-      <div className="content product">
-        <div className="col-left">
-          <ImageDisplay
-            image={product.image}
-            imageAlt={product.imageAlt}
-            discount={product.discount}
-          />
-        </div>
-        <div className="col-right">
-          <InformationList
-            title={product.title}
-            price={product.price}
-            discount={product.discount}
-            salePrice={product.salePrice}
-            rating={product.rating}
-            description={product.description}
-          />
-          <ProductForm
-            product={product}
-            handleToast={handleToastNotification}
-          />
-        </div>
-      </div>
+      <Footer />
     </>
   );
 }
